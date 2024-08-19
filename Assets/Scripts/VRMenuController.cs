4b2dc03f9e42e337f71d22ecc4659067d4ef55c4
lukepham-holmesglen <54011967+lukepham-holmesglen@users.noreply.github.com>
2024-08-19 12:41:44 +1000

    Adding additional scripts to have UI menu face camera and have toggle through trigger press.

---
+++ Assets/Scripts/VRMenuController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class VRMenuController : MonoBehaviour
+{
+    public Canvas menuCanvas;  // Reference to the Canvas
+
+    void Start()
+    {
+        // Ensure the canvas is inactive initially
+        menuCanvas.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Always activate the menu on input
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))  // For Oculus
+        {
+            ActivateMenu();
+        }
+
+        if (Input.GetMouseButtonDown(0))  // 0 is the left mouse button
+        {
+            ActivateMenu();
+        }
+    }
+
+    void ActivateMenu()
+    {
+        // Always set the canvas as active
+        menuCanvas.gameObject.SetActive(true);
+    }
+
+    public void CloseMenu()
+    {
+        // Method to close the menu from within the menu
+        menuCanvas.gameObject.SetActive(false);
+    }
+}
