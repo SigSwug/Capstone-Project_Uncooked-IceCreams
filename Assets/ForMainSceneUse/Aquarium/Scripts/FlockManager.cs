93bad0469287ea97feaf0144bca0881499f483ab
SigSwug <bodhijames03@gmail.com>
2024-10-14 12:59:38 +1000

    Add a flocking system, and basic movement for the fish in the aquarium

---
+++ Assets/ForMainSceneUse/Aquarium/Scripts/FlockManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    public static FlockManager FM;
+
+    public GameObject fishPrefab;
+    public int numFish = 5;
+    public GameObject[] allFish;
+    public Vector3 swimLimits = new Vector3(1, 1, 1);
+    public Vector3 goalPos = Vector3.zero;
+
+    [Header("Fish Settings")]
+    [Range(0.0f, 5.0f)]
+    public float minSpeed;
+    [Range(0.0f, 5.0f)]
+    public float maxSpeed;
+    [Range(0.0f, 10.0f)]
+    public float neighbourDistance;
+    [Range(0.0f, 5.0f)]
+    public float rotationSpeed;
+
+
+
+
+    void Start()
+    {
+        allFish = new GameObject[numFish];
+
+        for (int i = 0; i < numFish; i++)
+        {
+            Vector3 pos = this.transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x), 
+                                                                Random.Range(-swimLimits.y, swimLimits.y), 
+                                                                Random.Range(-swimLimits.z, swimLimits.z));
+            allFish[i] = Instantiate(fishPrefab, pos, Quaternion.identity);
+        }
+        FM = this;
+
+        goalPos = this.transform.position;
+    }
+
+    void Update()
+    {
+        if (Random.Range(0, 5000) < 10)
+        {
+            goalPos = this.transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x),
+                                                            Random.Range(-swimLimits.y, swimLimits.y),
+                                                            Random.Range(-swimLimits.z, swimLimits.z));
+
+        }
+    }
+}
