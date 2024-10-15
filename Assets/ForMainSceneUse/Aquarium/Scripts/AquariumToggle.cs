aa90b141e589c12aac86a55c9d241f9705cb2dc7
SigSwug <bodhijames03@gmail.com>
2024-10-16 09:44:36 +1000

    Added an aquarium toggle to the UI menu

---
+++ Assets/ForMainSceneUse/Aquarium/Scripts/AquariumToggle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AquariumToggle : MonoBehaviour
+{
+    public GameObject aquarium;
+    public GameObject aquariumCover;
+
+    void Start()
+    {
+        
+    }
+
+    public void AquariumCoverToggle()
+    {
+        if (aquarium.activeInHierarchy == true)
+        {
+            aquarium.SetActive(false);
+            aquariumCover.SetActive(true);
+        }
+        else if (aquarium.activeInHierarchy == false)
+        {
+            aquarium.SetActive(true);
+            aquariumCover.SetActive(false);
+        }
+    }
+}
